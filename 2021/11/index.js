adb889711c745aa63861ff17dfaadfe8913b1362
Andrew Ruslander <andrew.ruslander@gmail.com>
2021-12-12 21:57:46 -0500

    Added Day 11

---
+++ 2021/11/index.js
@@ -0,0 +1,123 @@
+const { request } = require('../util/request');
+
+// Recursively searches a location and its surrounding locations
+const search = (map, visited, row, col, basinSize) => {
+  const rowMax = map.length - 1;
+  const colMax = map[0].length - 1;
+
+  const height = map[row][col];
+
+  // If our location is a 9 or we have already visited it, just skip over
+  if (height < 9 && visited[row][col] !== '*') {
+    // Mark this location as visited
+    visited[row][col] = '*';
+
+    // Increment the basin size
+    basinSize++;
+
+    // Search up
+    if (row > 0 && visited[row - 1][col] !== '*')
+      basinSize = search(map, visited, row - 1, col, basinSize);
+    // Search right
+    if (col < colMax && visited[row][col + 1] !== '*')
+      basinSize = search(map, visited, row, col + 1, basinSize);
+    // Search down
+    if (row < rowMax && visited[row + 1][col] !== '*')
+      basinSize = search(map, visited, row + 1, col, basinSize);
+    // Search left
+    if (col > 0 && visited[row][col - 1] !== '*')
+      basinSize = search(map, visited, row, col - 1, basinSize);
+  }
+
+  // Return our current basin size from this search
+  return basinSize;
+}
+
+request(11).then((response) => {
+  const { data, status } = response;
+
+  if (status === 200) {
+    let map = data.split('\n')
+      .filter((x) => x)
+      .map((x) => Array.from(x).map((y) => parseInt(y)));
+
+    const rowMax = map.length;
+    const colMax = map[0].length;
+
+    // ---- PART 1 ----
+
+    let part1Answer = 0;
+    let part2Answer = 0;
+    let megaFlash = false;
+
+    // Megaflash is when all the octopi flash as one!
+    while (!megaFlash) {
+      part2Answer++;
+
+      // Increment all positions by one
+      map = map.map((row) => {
+        return row.map((col) => col + 1);
+      });
+
+      // Loop until there is no more flashes
+      let flash = true;
+      while (flash) {
+        flash = false;
+
+        // Loop through each position in the map
+        for (let rowIndex = 0; rowIndex < rowMax; rowIndex++) {
+          for (let colIndex = 0; colIndex < colMax; colIndex++) {
+            const octopus = map[rowIndex][colIndex];
+
+            // If the octopus has reached critical mass, it's time to flash
+            if (octopus > 9) {
+              // For Part 1, we only want to tally everything up to step 100
+              if (part2Answer <= 100) part1Answer++;
+
+              // Set the toggle to true so we take another pass through to catch any
+              // octopi that flash due to this flash
+              flash = true;
+
+              // Set the octopus who just flashed to -1
+              map[rowIndex][colIndex] = -1;
+
+              // Increment all the surrounding octopi by one (if they haven't already flashed)
+              if (rowIndex > 0) {
+                if (colIndex > 0 && map[rowIndex - 1][colIndex - 1] > -1) map[rowIndex - 1][colIndex - 1] += 1;
+                if (map[rowIndex - 1][colIndex] > -1) map[rowIndex - 1][colIndex] += 1;
+                if (colIndex < colMax - 1 && map[rowIndex - 1][colIndex + 1] > -1) map[rowIndex - 1][colIndex + 1] += 1;
+              }
+
+              if (colIndex > 0 && map[rowIndex][colIndex - 1] > -1) map[rowIndex][colIndex - 1] += 1;
+              if (colIndex < colMax - 1 && map[rowIndex][colIndex + 1] > -1) map[rowIndex][colIndex + 1] += 1;
+
+              if (rowIndex < rowMax - 1) {
+                if (colIndex > 0 && map[rowIndex + 1][colIndex - 1] > -1) map[rowIndex + 1][colIndex - 1] += 1;
+                if (map[rowIndex + 1][colIndex] > -1) map[rowIndex + 1][colIndex] += 1;
+                if (colIndex < colMax - 1 && map[rowIndex + 1][colIndex + 1] > -1) map[rowIndex + 1][colIndex + 1] += 1;
+              }
+            }
+          }
+        }
+      }
+
+      // Check to see if every octopus flashed - this is the end of our loop if so
+      megaFlash = map.every((row) => row.every((col) => col === -1));
+
+      // Reset any octopus who flashed to 0
+      map = map.map((row) => {
+        return row.map((col) => col === -1 ? 0 : col);
+      });
+    }
+
+    console.log(`The answer to part 1 is ${part1Answer}`);
+
+    console.log(`The answer to part 2 is ${part2Answer}`);
+  } else {
+    console.log('Something terrible has happened...', response);
+  }
+
+  console.log('All done!');
+}).catch((err) => {
+  console.log('Something terrible has happened...', err);
+});
